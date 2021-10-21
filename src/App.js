e680e9e7c836cb93a0f1280e86b4cffbc2696e76
Richard Toney <tonyrichards22@gmail.com>
2021-10-21 02:26:46 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './Richard.png';
-import './App.css';
+import React from 'react'
+import LinkContainer from './components/LinkContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        My name is Richard and I'm going to master React!
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/DaOddOneOut"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check Me Out!
-        </a>
-      </header>
+        <LinkContainer />
     </div>
   );
 }
